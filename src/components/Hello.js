bf0dac5de2176910d58dd906ec70fe06afce3307
Henry C Anozie <henrycanozie1@gmail.com>
2026-09-23 15:16:03 +0100

    done with the JSX tutorial video

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,10 +1,12 @@
 import React from "react";
 const Hello = () => {
-    return (
-        <div>
-            <h1> Hello Vishaw</h1>
-        </div>
-    )
+   // return (
+    //    <div>
+     //       <h1> Hello Vishaw</h1>
+     //   </div>
+   // )
+   return React.createElement('div',{id: 'hello-div',className
+    : 'dummyclass'}, React.createElement('h1',null, 'Hello Houston'))
 }
 
 export default Hello
