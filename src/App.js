e3ad7e35e8a3db16c463f8b21a36aa71fcf31995
Henry C Anozie <henrycanozie1@gmail.com>
2026-09-25 13:49:27 +0100

    two props and children

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import { Greet } from './components/Greet';
+import Greet  from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
 function App() {
   return (
     <div className="App">
-   <Greet name="Alice"/>
-    <Greet name="Bob"/>
-     <Greet name="Charlie"/>
-    {/*<Welcome />*/}
+   <Greet name="Bruce" heroname="Batman">This is children props</Greet>
+    <Greet name="Tony Stark" heroname="Iron Man"/>
+     <Greet name="Peter Parker" heroname="Spider-Man"/>
+    <Welcome  name="Bruce" heroname="Batman" />
+    <Welcome  name="Tony Stark" heroname="Iron Man" />
+    <Welcome  name="Peter Parker" heroname="Spider-Man" />
     {/*<Hello />*/}
     </div>
   );
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -5,6 +5,12 @@ import React from 'react'
   //  return <h1>Hello Houston</h1>
 //}
 
-export const Greet = () =>  <h1>Hello Houston</h1>
+ const Greet = (props) =>{  
+  console.log(props)
+return(
+  <div>
+<h1>Hello {props.name} a.k.a {props.heroname}</h1>
+{props.children}</div>
+)} 
 
-// export default Greet
+export default Greet
